b9c742f07e18ad31fedaf0f7c18f1da41720814c
vitvr <victor.verde.vv@outlook.com>
2024-03-20 13:34:36 +0400

    Added functionality to quantity buttons, + misc.

--- ecom/urls.py
+++ ecom/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from sales.views import (CartListView,add_to_cart,load_cart_items,load_wishlist,CategoryView)
+from sales.views import (CartListView,add_to_cart,add_to_wishlist,load_cart_items,load_wishlist,CategoryView, change_quantity)
 
 from users.views import (LandingPageView, 
                          SignupView, 
@@ -48,12 +48,14 @@ urlpatterns = [
     path('', ItemListView.as_view(), name='landing-page'),
     path('search/', SearchView.as_view(), name='products-list'),
     path('add_to_cart/<int:id>/',sales.views.add_to_cart,name='add_to_cart'),
+    path('add_to_wishlist/<int:id>/',sales.views.add_to_wishlist,name='add_to_wishlist'),
     path("our-services/", ServicesView.as_view(), name='services'),
     path("vision/", VisionView.as_view(), name='vision'),
     path("our-team/", OurTeamView.as_view(), name='team'),
     path('cart/', CartListView.as_view(), name='cart'),
     path('ajax/load_cart_list/', load_cart_items, name='ajax_load_cart_list'),
     path('ajax/load_wishlist/', load_wishlist, name='ajax_load_wishlist'),
+    path('ajax/change_quantity/<int:id>/', change_quantity, name='ajax_change_quantity'),
     path('signup/', SignupView.as_view(), name='signup'),
     path('membership-plans/', MembershipPlanView.as_view(), name='member-plan'),
     path('membership-plans/payment', PaymentView.as_view(), name='payment'),
